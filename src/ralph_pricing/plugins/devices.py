7faa8987cf7ea6f652a5dbcf37f8d9bf4e641fc3
Radomir Dopieralski <radomir.dopieralski@allegro.pl>
2013-04-23 12:27:11 +0200

    Add plugin for updating devices from Ralph.

---
+++ src/ralph_pricing/plugins/devices.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from ralph.util import plugin, api_pricing
+from ralp_pricing.models import Device, Venture
+
+
+def update_device(data):
+    device, created = Device.objects.get_or_create(
+        device_id=data['id'],
+    )
+    device.name = data['name']
+    device.is_virtual = data['is_virtual']
+    device.is_blade = data['is_blade']
+    venture, venture_created = Venture.objects.get_or_create(
+        venture_id = data['venture_id']
+    )
+    device.venture = venture
+    parent, parent_created = Device.objects.get_or_create(
+        device_id=data['parent_id'],
+    )
+    return created + parent_created
+
+
+@plugin.register(chain='pricing', requires=['ventures'])
+def sync_devices(**kwargs):
+    """Updates the devices from Ralph."""
+
+    count = sum(update_device(data) for data in api_pricing.get_devices())
+    return True, '%d new devices' % count, kwargs
+
