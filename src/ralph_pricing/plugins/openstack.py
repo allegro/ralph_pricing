8b5a467973399ab1aca996d2543d0f2f657c8686
Kamil Warguła <kwargula@gmail.com>
2013-05-17 08:24:20 +0200

    Separate get_ventures functions

--- src/ralph_pricing/plugins/openstack.py
+++ src/ralph_pricing/plugins/openstack.py
@@ -11,7 +11,6 @@ import re
 
 
 from django.conf import settings
-from lck.django.common import nested_commit_on_success
 
 from ralph.util import plugin
 from ralph_pricing.models import UsageType, Venture, DailyUsage
@@ -21,9 +20,15 @@ from ralph_pricing.openstack import OpenStack
 VENTURE = re.compile('venture:(?P<venture>.*);')
 
 
-@nested_commit_on_success
-def set_usages(venture, data):
-    venture = Venture.objects.get(symbol=venture)
+class VentureDoesNotExistError(Exception):
+    pass
+
+
+def set_usages(venture, data, date):
+    try:
+        venture = Venture.objects.get(symbol=venture)
+    except Venture.DoesNotExist:
+        raise VentureDoesNotExistError('Venture: %s does not exist' % venture)
 
     def set_usage(name, key, venture, multiplier):
         if key not in data:
@@ -42,7 +47,7 @@ def set_usages(venture, data):
         usage.save()
     if venture:
         set_usage('OpenStack 10000 Memory GiB Hours', 'total_memory_mb_usage',
-            venture, 1024)
+            venture, 1024,)
         set_usage('OpenStack 10000 CPU Hours', 'total_vcpus_usage',
             venture, 1)
         set_usage('OpenStack 10000 Disk GiB Hours', 'total_local_gb_usage',
@@ -53,15 +58,30 @@ def set_usages(venture, data):
             venture, 1)
 
 
+def get_ventures(stack):
+    url = settings.OPENSTACK_TENANTS_URL
+    ventures = {}
+    result = stack.query('tenants', url=url, limit=1000)
+    for data in result['tenants']:
+        if data['enabled']:
+            venture = None
+            description = data.get('description')
+            if description:
+                result = re.match(VENTURE, description)
+                if result:
+                    venture = result.group('venture')
+            ventures[data['id']] = venture
+    return ventures
+
+
 @plugin.register(chain='pricing', requires=['sync_ventures'])
 def openstack(**kwargs):
     """Updates OpenStack usage per Venture"""
     if settings.OPENSTACK_URL is None:
         return False, 'not configured.', kwargs
     tenants = collections.defaultdict(lambda: collections.defaultdict(dict))
-    end = datetime.datetime.today().replace(
-        hour=0, minute=0, second=0, microsecond=0
-    )
+    date = kwargs['today']
+    end = date
     start = end - datetime.timedelta(days=1)
     ventures = {}
     for region in getattr(settings, 'OPENSTACK_REGIONS', ['']):
@@ -74,32 +94,16 @@ def openstack(**kwargs):
         for data in stack.simple_tenant_usage(start, end):
             tenants[data['tenant_id']][region].update(data)
     for url, query in getattr(settings, 'OPENSTACK_EXTRA_QUERIES', []):
-        if query == 'tenants':
-            result = stack.query(query, url=url, limit=1000)
-            for data in result['tenants']:
-                if data['enabled']:
-                    venture = None
-                    description = data.get('description')
-                    if description:
-                        result = re.match(VENTURE, description)
-                        if result:
-                            venture = result.group('venture')
-                    ventures[data['id']] = venture
-        else:
-            for data in stack.query(query, url=url,
-                    start=start.strftime('%Y-%m-%dT%H:%M:%S'),
-                    end=end.strftime('%Y-%m-%dT%H:%M:%S'),
-                ):
-                tenants[data['tenant_id']][url].update(data)
+        for data in stack.query(query, url=url,
+                start=start.strftime('%Y-%m-%dT%H:%M:%S'),
+                end=end.strftime('%Y-%m-%dT%H:%M:%S'),
+            ):
+            tenants[data['tenant_id']][url].update(data)
+    ventures = get_ventures(stack)
     for tenant_id, regions in tenants.iteritems():
         for region, data in regions.iteritems():
             venture = ventures.get(data['tenant_id'])
             if venture:
-                set_usages(venture, data)
-    return True, 'Openstack usages was saved', kwargs
-
-
-
-
-
+                set_usages(venture, data, date)
+    return True, 'Openstack usages ware saved', kwargs
 
