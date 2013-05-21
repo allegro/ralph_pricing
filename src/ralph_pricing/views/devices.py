2a46acbbe854511d395b9a1c722b349b664b4a1c
Wojciech Matyskiewicz <wojtek@matyskiewicz.com>
2013-05-21 09:25:49 +0200

    async report device

--- src/ralph_pricing/views/devices.py
+++ src/ralph_pricing/views/devices.py
@@ -5,17 +5,16 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from django.shortcuts import get_object_or_404, get_list_or_404
 from django.contrib import messages
 from django.http import HttpResponseRedirect
-
+from django.shortcuts import get_object_or_404, get_list_or_404
 from django.utils.translation import ugettext_lazy as _
 
-from ralph_pricing.views.reports import Report, currency
-from ralph_pricing.models import Venture
+from ralph_pricing.forms import DateRangeVentureForm
 from ralph_pricing.menus import ventures_menu
 from ralph_pricing.models import DailyDevice
-from ralph_pricing.forms import DateRangeVentureForm
+from ralph_pricing.models import Venture
+from ralph_pricing.views.reports import Report, currency
 
 
 class Devices(Report):
@@ -25,22 +24,31 @@ class Devices(Report):
 
     @staticmethod
     def get_data(start, end, venture, **kwargs):
-        data = []
-        if venture:
-            devices = DailyDevice.objects.filter(pricing_venture=venture)
-            total_count = devices.count()
-            for i, device in enumerate(devices):
-                row = [
-                        device.name,
-                        device.pricing_device.sn,
-                        device.pricing_device.barcode,
-                        device.is_deprecated,
-                        currency(
-                            device.get_devices_price(start, end, venture)
-                        ),
-                    ]
-                progress = (100 * i) // total_count
-                yield progress, row
+        if not venture:
+            return
+        devices_ids = DailyDevice.objects.filter(
+            date__gte=start,
+            date__lte=end,
+            pricing_venture=venture,
+        ).values_list('pricing_device', flat=True).distinct()
+        total_count = devices_ids.count()
+        for i, id in enumerate(devices_ids):
+            device = DailyDevice.objects.get(pricing_device=id)
+            row = [
+                    device.name,
+                    device.pricing_device.sn,
+                    device.pricing_device.barcode,
+                    device.is_deprecated,
+                    currency(
+                        device.pricing_device.get_device_price(
+                            start,
+                            end,
+                            venture,
+                        )
+                    ),
+                ]
+            progress = (100 * i) // total_count
+            yield progress, row
 
     @staticmethod
     def get_header(**kwargs):
