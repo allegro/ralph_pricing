45b6b0d1ce32ff1eb33d604682a78936490b2825
Wojciech Matyśkiewicz <wojtek@matyskiewicz.com>
2013-08-05 15:34:25 +0200

    refactoring get_or_create in transaction

--- src/ralph_pricing/plugins/assets.py
+++ src/ralph_pricing/plugins/assets.py
@@ -13,6 +13,7 @@ from ralph_pricing.models import Device, DailyDevice
 
 @commit_on_success
 def update_assets(data, date):
+    created = False
     if not data['ralph_id']:
         return False
     try:
@@ -26,9 +27,12 @@ def update_assets(data, date):
     else:
         old_device.asset_id = None
         old_device.save()
-    device, created = Device.objects.get_or_create(
-        device_id=data['ralph_id'],
-    )
+    try:
+        device = Device.objects.get(device_id=data['ralph_id'])
+    except Device.DoesNotExist:
+        created = True
+        device = Device()
+        device.device_id = data['ralph_id']
     device.asset_id = data['asset_id']
     device.slots = data['slots']
     device.sn = data['sn']
