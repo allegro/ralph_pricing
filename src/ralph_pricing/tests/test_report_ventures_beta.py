5594d0ee58edd0c00a15856c4cecc7c5eed5c26d
Mateusz Kurek <master.mateusz@gmail.com>
2014-02-18 15:52:05 +0100

    ventures beta report tests

--- src/ralph_pricing/tests/test_report_ventures_beta.py
+++ src/ralph_pricing/tests/test_report_ventures_beta.py
@@ -6,6 +6,9 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import datetime
+import mock
+from collections import OrderedDict
+from decimal import Decimal as D
 
 from django.test import TestCase
 
@@ -18,15 +21,17 @@ class TestReportVenturesBeta(TestCase):
         self.day = day = datetime.date(2013, 4, 25)
 
         # ventures
-        self.venture = models.Venture(venture_id=1, name='a', is_active=True)
+        self.venture = models.Venture(venture_id=1, name='b', is_active=True)
         self.venture.save()
-        subventure = models.Venture(
+        self.subventure = models.Venture(
             venture_id=2,
             parent=self.venture,
-            name='b',
+            name='bb',
             is_active=False,
         )
-        subventure.save()
+        self.subventure.save()
+        self.venture2 = models.Venture(venture_id=3, name='a', is_active=True)
+        self.venture2.save()
 
         # devices (assets)
         device = models.Device(
@@ -53,7 +58,7 @@ class TestReportVenturesBeta(TestCase):
             date=day,
             name='ziew',
             price=833833,
-            pricing_venture=subventure,
+            pricing_venture=self.subventure,
         )
         other_daily.save()
 
@@ -62,7 +67,7 @@ class TestReportVenturesBeta(TestCase):
         self.warehouse.save()
 
         # usages
-        usage_type = models.UsageType(name='ut1')
+        usage_type = models.UsageType(name='UT1', symbol='ut1')
         usage_type.save()
         daily_usage = models.DailyUsage(
             type=usage_type,
@@ -71,13 +76,19 @@ class TestReportVenturesBeta(TestCase):
             pricing_venture=self.venture,
         )
         daily_usage.save()
-        usage_type2 = models.UsageType(name='ut2')
-        usage_type2.save()
+        usage_type3 = models.UsageType(
+            name='UT2',
+            symbol='ut2',
+            show_in_report=False,
+        )
+        usage_type3.save()
 
         # usages by warehouse
         warehouse_usage_type = models.UsageType(
-            name='waciki2',
+            name='UT3',
+            symbol='ut3',
             by_warehouse=True,
+            order=3,
         )
         warehouse_usage_type.save()
         daily_warehouse_usage = models.DailyUsage(
@@ -89,50 +100,161 @@ class TestReportVenturesBeta(TestCase):
         )
         daily_warehouse_usage.save()
 
-    def test_get_usage_types(self):
+    def test_get_plugins(self):
         """
-        Test if usage types are correctly filtered and ordered
+        Test plugins list based on visible usage types
         """
-        pass
+        plugins = AllVentures._get_plugins()
+        self.assertEquals(plugins, [
+            dict(name='Information', symbol='information'),
+            dict(name='Deprecation', symbol='deprecation'),
+            dict(name='UT3', symbol='ut3'),  # order matters
+            dict(name='UT1', symbol='ut1'),
+        ])
 
     def test_get_as_currency(self):
         """
         Test if decimal is properly 'transformed' to currency
         """
-        pass
+        currency = AllVentures._get_as_currency('1234', False)
+        self.assertEquals(currency, ('1234.00 PLN', D(0)))
 
-    def test_prepare_field(self):
+    def test_get_as_currency_total_cost(self):
         """
-        Test if field is properly prepared for placing it in report
+        Test if total cost decimal is properly 'transformed' to currency
         """
-        pass
+        currency = AllVentures._get_as_currency(1234, True)
+        self.assertEquals(currency, ('1234.00 PLN', D('1234')))
 
-    def test_prepare_row(self):
+    def test_prepare_field_value_in_venture_data(self):
+        """
+        Test if field is properly prepared for placing it in report. Value
+        in venture data.
+        """
+        venture_data = {
+            'field1': '1234',
+        }
+        rules = {
+            'currency': False
+        }
+        result = AllVentures._prepare_field('field1', rules, venture_data)
+        self.assertEquals(result, ('1234', D(0)))
+
+    def test_prepare_field_value_in_venture_data_currency(self):
+        """
+        Test if field is properly prepared for placing it in report. Value
+        in venture data.
+        """
+        venture_data = {
+            'field1': 1234,
+        }
+        rules = {
+            'currency': True,
+            'total_cost': True,
+        }
+        result = AllVentures._prepare_field('field1', rules, venture_data)
+        self.assertEquals(result, ('1234.00 PLN', D('1234')))
+
+    def test_prepare_field_value_not_in_venture_date_default(self):
+        """
+        Test if field is properly prepared for placing it in report. Value not
+        in venture data and there is no default rule.
+        """
+        venture_data = {}
+        rules = {
+            'currency': True,
+            'total_cost': True,
+            'default': 3,
+        }
+        result = AllVentures._prepare_field('field1', rules, venture_data)
+        self.assertEquals(result, ('3.00 PLN', D('3')))
+
+    def test_prepare_field_value_not_in_venture_date(self):
+        """
+        Test if field is properly prepared for placing it in report. Value not
+        in venture data and there is default rule.
+        """
+        venture_data = {}
+        rules = {
+            'currency': True,
+            'total_cost': True,
+        }
+        result = AllVentures._prepare_field('field1', rules, venture_data)
+        self.assertEquals(result, ('0.00 PLN', D('0')))
+
+    def test_prepare_field_value_basestring(self):
+        """
+        Test if field is properly prepared for placing it in report. Value is
+        string.
+        """
+        venture_data = {
+            'field1': '123'
+        }
+        rules = {
+            'currency': True,
+            'total_cost': True,
+        }
+        result = AllVentures._prepare_field('field1', rules, venture_data)
+        self.assertEquals(result, ('123', D('0')))
+
+    def _sample_schema(self):
+        return [
+            OrderedDict([
+                ('field1', {'name': 'Field1'}),
+                ('field2', {'name': 'Field2', 'currency': True}),
+            ]),
+            OrderedDict([
+                ('field3', {'name': 'Field3'}),
+                ('field4', {'name': 'Field4'}),
+            ]),
+        ]
+
+    @mock.patch.object(AllVentures, '_get_schema')
+    def test_prepare_row(self, get_schema_mock):
         """
         Test if whole row is properly prepared for placing it in report
         """
-        pass
+        venture_data = {
+            'field1': 123,
+            'field2': 34,
+            'field3': 3123,
+            'field4': 3434
+        }
+        get_schema_mock.return_value = self._sample_schema()
+        result = AllVentures._prepare_venture_row(venture_data)
+        self.assertEquals(result, [123, u'34.00 PLN', 3123, 3434, u'0.00 PLN'])
 
     def test_get_ventures(self):
         """
         Test if ventures are correctly filtered
         """
-        pass
+        def get_id(l):
+            return [i.id for i in l]
+
+        ventures1 = AllVentures._get_ventures(is_active=True)
+        self.assertEquals(get_id(ventures1),
+                          get_id([self.venture2, self.venture]))
+
+        ventures1 = AllVentures._get_ventures(is_active=False)
+        self.assertEquals(
+            get_id(ventures1),
+            get_id([self.venture2, self.venture, self.subventure])
+        )
 
     def test_get_report_data(self):
         """
         Test generating data for whole report
         """
-        pass
+        raise NotImplementedError()
 
-    def test_get_schema(self):
-        """
-        Test getting full schema for report
-        """
-        pass
-
-    def test_get_header(self):
+    @mock.patch.object(AllVentures, '_get_schema')
+    def test_get_header(self, get_schema_mock):
         """
         Test getting headers for report
         """
-        pass
+        get_schema_mock.return_value = self._sample_schema()
+        result = AllVentures.get_header()
+        self.assertEquals(
+            result,
+            ['Field1', 'Field2', 'Field3', 'Field4', 'Total cost']
+        )
--- src/ralph_pricing/views/ventures_beta.py
+++ src/ralph_pricing/views/ventures_beta.py
@@ -7,8 +7,8 @@ from __future__ import unicode_literals
 
 import logging
 from decimal import Decimal as D
-from lck.cache import memoize
 
+from lck.cache import memoize
 from django.utils.translation import ugettext_lazy as _
 
 from ralph_pricing.views.reports import Report
@@ -49,7 +49,7 @@ class AllVenturesBeta(Report):
     def _get_plugins(cls):
         base_plugins = [
             AttributeDict(name='Information', symbol='information'),
-            AttributeDict(name='Deprecation', symbol='deprecation')
+            AttributeDict(name='Deprecation', symbol='deprecation'),
         ]
         usage_types = [
             AttributeDict(
