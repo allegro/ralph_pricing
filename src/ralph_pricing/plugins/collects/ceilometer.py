01ce517400864dca389988bd5dada821c6d7711b
Michał Jastrzębski <inc007@gmail.com>
2014-04-01 11:14:59 +0200

    instances in ceilometer plugin

--- src/ralph_pricing/plugins/collects/ceilometer.py
+++ src/ralph_pricing/plugins/collects/ceilometer.py
@@ -5,13 +5,12 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import datetime
-import json
 import logging
 
-import requests
-
-from ceilometerclient.client import get_client
 from django.conf import settings
+from ceilometerclient.client import get_client
+from keystoneclient.v2_0 import client
+from novaclient.v1_1 import client as nova_client
 
 from ralph.util import plugin
 from ralph_pricing.models import UsageType, Venture, DailyUsage
@@ -20,39 +19,7 @@ from ralph_pricing.models import UsageType, Venture, DailyUsage
 logger = logging.getLogger('ralph')
 
 
-def get_token(site):
-    data = {
-        "auth": {
-            "passwordCredentials": {
-                "username": site['OS_USERNAME'],
-                "password": site['OS_PASSWORD'],
-            }
-        }
-    }
-    token = requests.post(
-        site['OS_AUTH_URL'] + "/tokens",
-        data=json.dumps(data),
-        headers={
-            'content-type': 'application/json',
-        },
-    )
-    return json.loads(str(token.content))['access']['token']['id']
-
-
-def get_tenants(site):
-    """
-    For some reason keystoneclient forcibly use admin endpoint.
-    """
-    headers_tenant = {
-        'X-Auth-Token': get_token(site),
-        'content-type': 'application/json',
-    }
-    tenants_url = site['OS_AUTH_URL'] + "/tenants"
-    tenants = requests.get(tenants_url, headers=headers_tenant)
-    return json.loads(str(tenants.content)).get('tenants', [])
-
-
-def get_ceilometer_usages(client, tenants, date=None):
+def get_ceilometer_usages(client, tenants, date=None, flavors=[]):
     """
     Function which talks with openstack
     """
@@ -69,31 +36,31 @@ def get_ceilometer_usages(client, tenants, date=None):
     statistics = {}
     for tenant in tenants:
         try:
-            tenant_venture = tenant['description'].split(";")[1]
-        except (ValueError, IndexError):
+            tenant_venture = tenant.description.split(";")[1]
+        except (ValueError, IndexError, AttributeError):
             logger.error(
                 "Tenant malformed: {}".format(str(tenant))
             )
             continue
         statistics[tenant_venture] = {}
+        query = [
+            {
+                'field': 'project_id',
+                'op': 'eq',
+                'value': tenant.id,
+            },
+            {
+                "field": "timestamp",
+                "op": "ge",
+                "value": yesterday.isoformat(),
+            },
+            {
+                "field": "timestamp",
+                "op": "lt",
+                "value": today.isoformat(),
+            },
+        ]
         for meter in meters:
-            query = [
-                {
-                    'field': 'project_id',
-                    'op': 'eq',
-                    'value': tenant['id'],
-                },
-                {
-                    "field": "timestamp",
-                    "op": "ge",
-                    "value": yesterday.isoformat(),
-                },
-                {
-                    "field": "timestamp",
-                    "op": "lt",
-                    "value": today.isoformat(),
-                },
-            ]
             logger.debug(
                 "stats {}-{} tenant: {} metric: {}".format(
                     yesterday.isoformat(),
@@ -105,11 +72,7 @@ def get_ceilometer_usages(client, tenants, date=None):
             try:
                 stats = client.statistics.list(meter_name=meter, q=query)[0]
             except IndexError:
-                logger.warning(
-                    "No statistics for tenant {}".format(
-                        tenant['id']
-                    )
-                )
+                statistics[tenant_venture][meter] = 0
                 continue
             if meter not in statistics[tenant_venture]:
                 statistics[tenant_venture][meter] = 0
@@ -120,6 +83,23 @@ def get_ceilometer_usages(client, tenants, date=None):
                 stats.sum,
                 stats.unit,
             ))
+        aggregates = [{'func': 'cardinality', 'param': 'resource_id'}]
+        logger.debug("Getting instance usage for tenant {}".format(
+            tenant_venture
+        ))
+        for flav in flavors:
+            meter = "instance:{}".format(flav)
+            stats = client.statistics.list(
+                meter,
+                q=query,
+                period=3600,
+                aggregates=aggregates,
+            )
+            icount = 0
+            for sample in stats:
+                icount += sample.aggregate.get('cardinality/resource_id', 0)
+            meter_name = 'instance.{}'.format(flav)
+            statistics[tenant_venture][meter_name] = icount
     return statistics
 
 
@@ -141,7 +121,11 @@ def save_ceilometer_usages(usages, date):
         )
         usage.value = value
         usage.save()
-        logger.info("Usages saved {}:{}".format(venture, value))
+        logger.info("Usages saved {},{}:{}".format(
+            venture,
+            "openstack." + usage_symbol,
+            value,
+        ))
 
     for venture_symbol, usages in usages.iteritems():
         try:
@@ -169,7 +153,14 @@ def ceilometer(**kwargs):
         logger.info("site {}".format(
             site['OS_METERING_URL'],
         ))
-        tenants = get_tenants(site)
+        ks = client.Client(
+            username=site['OS_USERNAME'],
+            password=site['OS_PASSWORD'],
+            tenant_name=site['OS_TENANT_NAME'],
+            auth_url=site['OS_AUTH_URL'],
+        )
+        tenants = ks.tenants.list()
+
         ceilo_client = get_client(
             "2",
             ceilometer_url=site['OS_METERING_URL'],
@@ -178,10 +169,19 @@ def ceilometer(**kwargs):
             os_auth_url=site['OS_AUTH_URL'],
             os_tenant_name=site['OS_TENANT_NAME'],
         )
+        nova = nova_client.Client(
+            site['OS_USERNAME'],
+            site['OS_PASSWORD'],
+            site['OS_TENANT_NAME'],
+            site['OS_AUTH_URL'],
+            service_type="compute",
+        )
+        flavors = [f.name for f in nova.flavors.list()]
         stats = get_ceilometer_usages(
             ceilo_client,
             tenants,
             date=kwargs['today'],
+            flavors=flavors,
         )
         save_ceilometer_usages(stats, date=kwargs['today'])
     return True, 'ceilometer usages saved', kwargs
