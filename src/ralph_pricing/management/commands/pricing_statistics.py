a79d71e2a27eea98a20afcdab6b31314e9681783
Kula <kula1922@gmail.com>
2014-04-10 15:09:13 +0200

    Remove model and plugin
    - Remove DailyStatistics model
    - Remove statistics plugi
    - Change pricing_statistics

--- src/ralph_pricing/management/commands/pricing_statistics.py
+++ src/ralph_pricing/management/commands/pricing_statistics.py
@@ -6,13 +6,14 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import logging
+import math
 import textwrap
 from datetime import datetime, date, timedelta
 from optparse import make_option
 
 from django.core.management.base import BaseCommand
 
-from ralph_pricing.models import DailyStatistics
+from ralph_pricing.models import UsageType, DailyUsage
 
 
 logger = logging.getLogger(__name__)
@@ -129,8 +130,103 @@ class Command(BaseCommand):
                 flag_settings['show_all'] = False
 
         self._draw_statistics(
-            DailyStatistics.compare_days(base, second),
+            self.compare_days(base, second),
             flag_settings,
             base,
             second,
         )
+
+    def get_statistics(self, date):
+        '''
+        Get data for given date and progress it to UsageName:Count format
+
+        :param datetime second_date: Date for whitch data will be select
+        :returns dict: Dict where key is usage type name and value is total
+        usages count
+        :rtype dict:
+        '''
+
+        results = {}
+        for usage in UsageType.objects.all():
+            results[usage.name] = DailyUsage.objects.filter(
+                date=date,
+                type=usage,
+            ).count()
+        return results
+
+    def compare_data(self, base_data, second_data):
+        '''
+        Compare two dict with data. Return differences betwen given data.
+
+        :param dict base_data: Statistics data for base date
+        :param dict second_data: Statistics data for second date
+        :returns dict: Dict with differences betwen given data
+        :rtype dict:
+        '''
+        results = {}
+        for key, value in base_data.iteritems():
+            results[key] = int(value) - int(second_data.get(key, 0))
+        return results
+
+    def get_warnings(self, base_data, differences_data, percent):
+        '''
+        Try to find any warnings. If data stroke are more then given percent
+        this should be marks as warning
+
+        :param dict base_data: Statistics data for base date
+        :param dict differences_data: Differences data
+        :param int percent: Percent for defining the warning limits
+        :returns dict: Usage types whitch can be errors
+        :rtype dict:
+        '''
+        results = {}
+        for key, value in base_data.iteritems():
+            differences_value = math.fabs(differences_data.get(key, 0))
+            if (value * percent / 100) < differences_value:
+                results[key] = differences_data.get(key, 0)
+        return results
+
+    def get_errors(self, base_data, defferences_data):
+        '''
+        Try to find any errors. If data from second_date are not null but
+        data from base_date are then we have error.
+
+        :param dict base_data: Statistics data for base date
+        :param dict differences_data: Differences data
+        :returns dict: Error usage types
+        :rtype dict:
+        '''
+        results = {}
+        for key, value in base_data.iteritems():
+            if value == 0 and defferences_data.get(key, 0) != 0:
+                results[key] = math.fabs(defferences_data[key])
+        return results
+
+    def compare_days(self, base_date, second_date, percent=50):
+        '''
+        Compare data from two given dates and try find errors or warnings
+
+        :param datetime base_date: first (higher) date
+        :param datetime second_date: second (lower) date
+        :param int percent: Percent for defining the warning limits
+        :returns dict: Statistics data
+        :rtype dict:
+        '''
+        results = {
+            'base': self.get_statistics(base_date),
+            'second': self.get_statistics(second_date),
+        }
+        results['differences'] = self.compare_data(
+            results['base'],
+            results['second'],
+        )
+        results['warnings'] = self.get_warnings(
+            results['base'],
+            results['differences'],
+            percent,
+        )
+        results['errors'] = self.get_errors(
+            results['base'],
+            results['differences'],
+        )
+        return results
