a06689258cf1a55613b8c192f420dad7f8b27964
Kula <kula1922@gmail.com>
2014-05-09 13:33:54 +0200

    fix flake8

--- src/ralph_pricing/tests/utils.py
+++ src/ralph_pricing/tests/utils.py
@@ -14,15 +14,18 @@ def get_or_create_device(name=None, device_id=None):
     name = name if name else "Default{0}".format(device_id)
     return Device.objects.get_or_create(name=name, device_id=device_id)[0]
 
+
 def get_or_create_venture(name=None, venture_id=None):
     venture_id = venture_id if venture_id else Venture.objects.all().count()
     name = name if name else "Default{0}".format(venture_id)
     return Venture.objects.get_or_create(name=name, venture_id=venture_id)[0]
 
+
 def get_or_create_extra_cost_type(name=None):
     if not name:
         name = "Default{0}".format(ExtraCostType.objects.all().count())
     return ExtraCostType.objects.get_or_create(name=name)[0]
+
 
 def get_or_create_daily_extra_cost(
     date=None,
@@ -30,7 +33,7 @@ def get_or_create_daily_extra_cost(
     type=None,
     value=None,
 ):
-    date = date if date else datetime.date(year=2014, month=5, day=1) 
+    date = date if date else datetime.date(year=2014, month=5, day=1)
     venture = venture if venture else get_or_create_venture()
     type = type if type else get_or_create_extra_cost_type()
     value = value if value else D(100)
