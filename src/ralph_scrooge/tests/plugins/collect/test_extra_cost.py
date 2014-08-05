1807990a00aa91e561b176934b07ea8bcd741a93
Slawomir Sawicki <ext_slawomir.sawicki@c50135.allegrogroup.internal>
2014-08-05 09:15:46 +0200

    Fix notics

--- src/ralph_scrooge/tests/plugins/collect/test_extra_cost.py
+++ src/ralph_scrooge/tests/plugins/collect/test_extra_cost.py
@@ -41,7 +41,7 @@ class TestExtraCostCollectPlugin(TestCase):
         )
 
     def test_extracost_when_update(self):
-        extra_cost(**{'today': self.date})
+        extra_cost(today=self.date)
         self.assertEqual(
             (True, u'0 new, 1 updated, 1 total'),
             extra_cost(**{'today': self.date})
