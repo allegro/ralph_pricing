18ea121c8fe80fcc7f2a21f1074c96127e485f02
Mateusz Kurek <master.mateusz@gmail.com>
2014-11-04 14:17:48 +0100

    fixed assets commands unit tests

--- src/ralph_scrooge/tests/management/commands/test_scrooge_asset_models.py
+++ src/ralph_scrooge/tests/management/commands/test_scrooge_asset_models.py
@@ -10,13 +10,8 @@ from django.test import TestCase
 from ralph_scrooge.management.commands.scrooge_asset_models import (
     Command
 )
-from ralph_assets.models import (
-    Asset,
-    DeviceInfo,
-    Warehouse,
-    AssetType,
-    AssetModel,
-)
+from ralph_assets.models import DeviceInfo
+from ralph_assets.tests.utils.assets import DCAssetFactory
 from ralph.discovery.models import Device as RalphDevice
 
 
@@ -28,15 +23,19 @@ class TestScroogeBaseCommand(TestCase):
         self.device_info = DeviceInfo.objects.create(
             ralph_device_id=1,
         )
-        self.asset = Asset.objects.create(
-            device_info=self.device_info,
-            type=AssetType.data_center,
-            model=AssetModel.objects.create(),
-            warehouse=Warehouse.objects.create()
-        )
+        self.asset = DCAssetFactory()
 
     def test_get_data(self):
         self.assertEqual(
             Command().get_data(),
-            [[u'', None, None, 0, 0.0, 0, None, 1]],
+            [[
+                self.asset.model.name,
+                self.asset.model.manufacturer.name,
+                self.asset.model.category.name,
+                self.asset.model.power_consumption,
+                self.asset.model.height_of_device,
+                self.asset.model.cores_count,
+                self.asset.model.type,
+                1,
+            ]],
         )
