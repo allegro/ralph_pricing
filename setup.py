1674e0c3ddd2abb82d704f1eb3cd5b0affe454b6
Tymoteusz Jankowski <tymoteusz.jankowski@ext.allegro.pl>
2015-06-01 14:32:36 +0200

    set ceilometerclient version to: python-ceilometerclient==1.0.10

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     install_requires=[
         'ralph>=2.3.1',
         'ralph_assets>=2.5.1',
-        'python-ceilometerclient>=1.0.10',
+        'python-ceilometerclient==1.0.10',
         'pymongo>=2.7.2',
         'python-novaclient==2.17.0',
         'django-simple-history',
