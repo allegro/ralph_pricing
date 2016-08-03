f4a2e3086aa3deb50a1096b4942843b6d521931b
Tomasz Mieszkowski <tomasz.mieszkowski@allegrogroup.com>
2016-08-03 15:37:57 +0200

    profit_center wip#1

--- src/ralph_scrooge/plugins/collect/profit_center.py
+++ src/ralph_scrooge/plugins/collect/profit_center.py
@@ -9,47 +9,43 @@ import logging
 
 from django.db.transaction import commit_on_success
 
-from ralph.util import plugin
-from ralph.util.api_scrooge import get_profit_centers
+from ralph.util import plugin  # XXX to be replaced later..?
 from ralph_scrooge.models import BusinessLine, ProfitCenter
+from ralph_scrooge.plugins.collect.utils import get_from_ralph
 
 
 logger = logging.getLogger(__name__)
 
 
 @commit_on_success
-def update_profit_center(data, date, default_business_line):
-    """
-    Updates single profit center according to data from ralph
-    """
-    if data['business_line'] is not None:
-        business_line = BusinessLine.objects.get(ci_id=data['business_line'])
+def update_profit_center(pc, default_business_line):
+    if pc['business_segment'] is not None:
+        business_line = BusinessLine.objects.get(
+            ci_id=pc['business_segment']['id']
+        )
     else:
         business_line = default_business_line
     profit_center, created = ProfitCenter.objects.get_or_create(
-        ci_id=data['ci_id'],
+        ci_id=pc['id'],
     )
-    profit_center.ci_uid = data['ci_uid']
-    profit_center.name = data['name']
-    profit_center.description = data['description']
+    profit_center.name = pc['name']
+    profit_center.description = pc['description']
     profit_center.business_line = business_line
     profit_center.save()
     return created
 
 
 @plugin.register(chain='scrooge', requires=['business_line'])
-def profit_center(today, **kwargs):
-    """
-    Updates Business Lines from CMDB
-    """
-    new_bl = total = 0
+def profit_center(**kwargs):
+    new_pc = total = 0
     default_business_line = BusinessLine.objects.get(pk=1)
-    for data in get_profit_centers():
-        if update_profit_center(data, today, default_business_line):
-            new_bl += 1
+    for pc in get_from_ralph("profit-centers", logger):
+        created = update_profit_center(pc, default_business_line)
+        if created:
+            new_pc += 1
         total += 1
     return True, '{} new profit center(s), {} updated, {} total'.format(
-        new_bl,
-        total - new_bl,
+        new_pc,
+        total - new_pc,
         total,
     )
