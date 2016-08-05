e11c0437deab5921d02f102f5e4383892edf1875
Tomasz Mieszkowski <tomasz.mieszkowski@allegrogroup.com>
2016-08-05 15:25:02 +0200

    tenant wip#1

--- src/ralph_scrooge/plugins/collect/tenant.py
+++ src/ralph_scrooge/plugins/collect/tenant.py
@@ -8,9 +8,8 @@ import logging
 
 from django.conf import settings
 
-from ralph.util import plugin, api_scrooge
+from ralph.util import plugin
 from ralph_scrooge.models import (
-    PricingObjectModel,
     PRICING_OBJECT_TYPES,
     ServiceEnvironment,
     TenantInfo,
@@ -18,53 +17,38 @@ from ralph_scrooge.models import (
 from ralph_scrooge.plugins.collect._exceptions import (
     UnknownServiceEnvironmentNotConfiguredError,
 )
+from ralph_scrooge.plugins.collect.utils import get_from_ralph
 
 logger = logging.getLogger(__name__)
 
 
-def get_model(ralph_tenant):
-    model = PricingObjectModel.objects.get_or_create(
-        model_id=ralph_tenant['model_id'],
-        type_id=PRICING_OBJECT_TYPES.TENANT,
-        defaults=dict(
-            name=ralph_tenant['model_name'],
-        )
-    )[0]
-    model.name = ralph_tenant['model_name']
-    model.save()
-    return model
-
-
-def save_tenant_info(ralph_tenant, unknown_service_environment):
+def save_tenant_info(ralph_tenant, unknown_service_env):
     created = False
     try:
         service_environment = ServiceEnvironment.objects.get(
-            service__ci_id=ralph_tenant['service_id'],
-            environment__ci_id=ralph_tenant['environment_id'],
+            # XXX should we get service env by service_id && env_id..?
+            id=ralph_tenant['service_env']['id']  # XXX what if service env will be empty?
         )
     except ServiceEnvironment.DoesNotExist:
         logger.warning(
-            'Invalid (or missing) service environment for tenant {}-{}'.format(
-                ralph_tenant['device_id'],
+            'Invalid (or missing) service environment for project {}'.format(
                 ralph_tenant['name']
             )
         )
-        service_environment = unknown_service_environment
+        service_environment = unknown_service_env
 
     try:
         tenant_info = TenantInfo.objects.get(
-            tenant_id=ralph_tenant['tenant_id'],
+            tenant_id=ralph_tenant['id'],
         )
     except TenantInfo.DoesNotExist:
         created = True
         tenant_info = TenantInfo(
-            tenant_id=ralph_tenant['tenant_id'],
+            tenant_id=ralph_tenant['id'],
             type_id=PRICING_OBJECT_TYPES.TENANT,
         )
-    tenant_info.model = get_model(ralph_tenant)
     tenant_info.name = ralph_tenant['name']
     tenant_info.remarks = ralph_tenant['remarks']
-    tenant_info.device_id = ralph_tenant['device_id']
     tenant_info.service_environment = service_environment
     tenant_info.save()
     return created, tenant_info
@@ -77,68 +61,59 @@ def save_daily_tenant_info(ralph_tenant, tenant_info, date):
     return daily_tenant_info
 
 
-def update_tenant(ralph_tenant, date, unknown_service_environment):
-    """
-    Updates single tenant info
-    """
+def update_tenant(ralph_tenant, date, unknown_service_env):
     created, tenant_info = save_tenant_info(
         ralph_tenant,
-        unknown_service_environment,
+        unknown_service_env,
     )
     save_daily_tenant_info(ralph_tenant, tenant_info, date)
     return created
 
 
-def get_unknown_service_environment(model_name):
-    """
-    Returns unknown service environment for OpenStack tenants
-    """
-    service_uid, environment_name = settings.UNKNOWN_SERVICES_ENVIRONMENTS.get(
-        'tenant', {}
-    ).get(model_name, (None, None))
-    unknown_service_environment = None
+def get_unknown_service_env():
+    service_uid, env_name = settings.UNKNOWN_SERVICES_ENVIRONMENTS.get(
+        'tenant', (None, None)
+    )
+    unknown_service_env = None
     if service_uid:
         try:
-            unknown_service_environment = ServiceEnvironment.objects.get(
+            unknown_service_env = ServiceEnvironment.objects.get(
                 service__ci_uid=service_uid,
-                environment__name=environment_name,
+                environment__name=env_name,
             )
         except ServiceEnvironment.DoesNotExist:
             pass
-    if not unknown_service_environment:
+    if not unknown_service_env:
         raise UnknownServiceEnvironmentNotConfiguredError()
-    return unknown_service_environment
+    return unknown_service_env
 
 
+# XXX rename tenant to project..?
 @plugin.register(chain='scrooge', requires=['service'])
 def tenant(today, **kwargs):
     new = total = 0
-    # check if all unknown SE are configured
-    for openstack_model in settings.OPENSTACK_TENANTS_MODELS:
-        try:
-            unknown_service_environment = get_unknown_service_environment(
-                openstack_model
-            )
-        except UnknownServiceEnvironmentNotConfiguredError:
-            msg = 'Unknown service environment not configured for {}'.format(
-                openstack_model
-            )
-            logger.error(msg)
-            return (False, msg)
+    try:
+        unknown_service_env = get_unknown_service_env()
+    except UnknownServiceEnvironmentNotConfiguredError:
+        msg = 'Unknown service environment not configured for "tenant"'
+        logger.error(msg)
+        return (False, msg)
 
-    for openstack_model in settings.OPENSTACK_TENANTS_MODELS:
-        unknown_service_environment = get_unknown_service_environment(
-            openstack_model
-        )
-        for ralph_tenant in api_scrooge.get_openstack_tenants(openstack_model):
-            total += 1
-            if update_tenant(
-                ralph_tenant,
-                today,
-                unknown_service_environment,
-            ):
-                new += 1
-    return True, 'Tenants: {0} new, {1} updated, {2} total'.format(
+    for provider in get_from_ralph("cloud-providers", logger):
+        if provider['name'].lower() == "openstack":
+            provider_id = provider['id']
+    if provider_id is None:
+        msg = "Can't find cloud provider for OpenStack in Ralph"
+        logger.error(msg)
+        return (False, msg)
+
+    query = "cloudprovider={}".format(provider_id)
+    for ralph_tenant in get_from_ralph("cloud-projects", logger, query=query):
+        created = update_tenant(ralph_tenant, today, unknown_service_env)
+        if created:
+            new += 1
+        total += 1
+    return True, '{} new tenants, {} updated, {} total'.format(
         new,
         total - new,
         total,
