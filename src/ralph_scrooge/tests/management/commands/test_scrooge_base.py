43ff3b4f9efc05c92685369d7444313ba7223515
Mateusz Kurek <mateusz.kurek@allegrogroup.com>
2016-09-06 08:28:01 +0200

    wip

--- src/ralph_scrooge/tests/management/commands/test_scrooge_base.py
+++ src/ralph_scrooge/tests/management/commands/test_scrooge_base.py
@@ -11,7 +11,7 @@ from mock import patch, ANY
 from django.core.management import call_command
 from django.test import TestCase
 
-from ralph_scrooge.management.commands.scrooge_asset_models import Command
+from ralph_scrooge.management.commands.scrooge_tenants_instances import Command
 from ralph_scrooge.management.commands._scrooge_base import (
     DEFAULT_CSV_ENCODING,
     DEFAULT_ENCODING,
@@ -26,7 +26,7 @@ class TestScroogeBaseCommand(TestCase):
     def test_get_prepared_data(self):
         self.assertEqual(
             Command().get_prepared_data(),
-            [Command.HEADERS, ['test']],
+            [Command().HEADERS, ['test']],
         )
 
     @patch.object(Command, 'get_data', lambda *a, **kw: [['test']])
@@ -61,7 +61,7 @@ class TestScroogeBaseCommand(TestCase):
         f = cStringIO.StringIO()
         call_command(COMMAND_NAME, stdout=f, encoding='cp1250')
         encoded = map(lambda x: x.encode('cp1250'), [
-            ';'.join(Command.HEADERS) + '\r\n',
+            ';'.join(Command().HEADERS) + '\r\n',
             ';'.join(['ĄŚĆŻÓŁŃĘĆ', 'ąśćżółńęć']) + '\r\n',
         ])
         # go to beginning of buffer
@@ -70,6 +70,7 @@ class TestScroogeBaseCommand(TestCase):
 
     def test_help(self):
         self.assertEquals(Command().help, (
-            "Generate report with assets and devices matching and basic "
-            "information\nabout asset, such as cores count, model name etc."
+            "Generate report with tenants instances and cost of particular "
+            "flavors\n(if tenants are billed based on ceilometer) or simple "
+            "usages."
         ))
