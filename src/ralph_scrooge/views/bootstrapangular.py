595e66892b415b332ab3c03be4e7dc16deba5b4f
Piotr Jarolewski <piotr.jarolewski@allegrogroup.com>
2016-09-07 14:39:24 +0200

    Remove not used views and templates

--- src/ralph_scrooge/views/bootstrapangular.py
+++ src/ralph_scrooge/views/bootstrapangular.py
@@ -6,24 +6,21 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 from django.views.generic import TemplateView
-from ralph.ui.views.common import MenuMixin
 
 from ralph_scrooge.app import Scrooge as app
 
 
-class BootstrapAngular(MenuMixin, TemplateView):
+class BootstrapAngular(TemplateView):
     """
     Initial view for bootstrap angularjs
     """
     template_name = 'ralph_scrooge/index.html'
-    submodule_name = 'scrooge'  # to satisfy MenuMixin, not used in angular
     module_name = app.module_name
 
 
-class BootstrapAngular2(MenuMixin, TemplateView):
+class BootstrapAngular2(TemplateView):
     """
     Initial view for bootstrap angularjs
     """
     template_name = 'ralph_scrooge/angular2.html'
-    submodule_name = 'scrooge'  # to satisfy MenuMixin, not used in angular
     module_name = app.module_name
