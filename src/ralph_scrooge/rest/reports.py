e0ae4afd21ea02ca08505705c7fdb95ee7e85be9
Mateusz Kurek <mkurek@users.noreply.github.com>
2016-09-08 10:05:46 +0200

    Remove unnecessary mgmt commands (#494)
    Remove unnecessary mgmt commands

--- src/ralph_scrooge/rest/reports.py
+++ src/ralph_scrooge/rest/reports.py
@@ -9,7 +9,7 @@ import dateutil
 import itertools
 import logging
 
-from bob.csvutil import make_csv_response
+from ralph_scrooge.csvutil import make_csv_response
 from rest_framework.exceptions import ParseError
 from rest_framework.response import Response
 from rest_framework.status import HTTP_204_NO_CONTENT
