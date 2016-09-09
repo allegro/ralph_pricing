b702a30973a9ce89b55b7ecd3097882672c0b7ae
Tomasz Mieszkowski <tomasz.mieszkowski@allegrogroup.com>
2016-09-09 14:58:17 +0200

    wip #2

--- src/ralph_scrooge/rest/pricing_service_usages.py
+++ src/ralph_scrooge/rest/pricing_service_usages.py
@@ -5,18 +5,165 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+import json
 import logging
+from collections import defaultdict
 
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.serializers import Serializer
+
+from ralph_scrooge.models import (
+    DailyUsage,
+    PricingObject,
+    # PricingService,  # XXX not used..?
+    ServiceEnvironment,
+    UsageType,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class PricingServiceUsageObject(object):
+    """
+    Container for pricing service resources usages per service
+    """
+    pricing_service = None
+    pricing_service_id = None  # XXX ?
+    date = None
+    overwrite = None
+    usages = []  # list of UsagesObject
+
+    def __init__(
+        self,
+        pricing_service=None,
+        pricing_service_id=None,
+        usages=None,
+        date=None,
+        **kwargs
+    ):
+        self.pricing_service = pricing_service
+        self.pricing_service_id = pricing_service_id
+        self.date = date
+        self.usages = usages or []
+        self.overwrite = 'no'  # default overwrite
+
+    # def to_dict(self, exclude_empty=False):
+    #     result = {
+    #         'date': self.date,
+    #         'pricing_service': self.pricing_service,
+    #         'pricing_service_id': self.pricing_service_id,
+    #         'overwrite': self.overwrite,
+    #         'usages': [u.to_dict(exclude_empty) for u in self.usages]
+    #     }
+    #     return result
+
+
+class PricingServiceUsageObjectSerializer(Serializer):
+    pricing_service = None
+    date = None
+    overwrite = None
+    usages = []  # list of UsagesObject
 
 
 class PricingServiceUsages(APIView):
 
     def post(self, request, *args, **kwargs):
+        post_data = json.loads(request.raw_post_data)
+        self.validate(post_data)
+        self.save_usages(post_data)
         result = {
             'status': 'ok',
             'message': 'success!',
         }
-        return Response(result)
+        return Response(result, status=status.HTTP_201_CREATED)
+
+    def validate(self, post_data):
+        # check if service exists
+        # check if date is properly set
+        # check if overwrite is properly set
+        pass
+
+    def get_service_env(self, usages):
+        se_params = {
+            'environment__name': usages.get('environment'),
+        }
+        if usages.service_id:
+            se_params['service_id'] = usages['service_id']
+        elif usages.service_uid:
+            se_params['service__ci_uid'] = usages['service_uid']
+        else:
+            se_params['service__name'] = usages['service']
+        return ServiceEnvironment.objects.get(**se_params)  # XXX try/except
+
+    def remove_previous_daily_usages(self, overwrite, date, usages_dp_objs):
+        if overwrite in ('values_only', 'delete_all_previous'):
+            logger.debug('Remove previous values ({})'.format(overwrite))
+            for k, v in usages_dp_objs.iteritems():
+                previuos_usages = DailyUsage.objects.filter(date=date, type=k)
+                if overwrite == 'values_only':
+                    previuos_usages = previuos_usages.filter(
+                        daily_pricing_object__in=v
+                    )
+                previuos_usages.delete()
+
+    def get_pricing_object(self, usages):
+        if usages.get('pricing_object'):
+            pricing_object = PricingObject.objects.get(
+                name=usages['pricing_object'],
+            )  # XXX try/except
+        else:
+            if not usages.get('environment') or not any([
+                usages.get('service_id'),
+                usages.get('service'),
+                usages.get('service_uid'),
+            ]):
+                pass
+                # XXX raise some exception here
+            else:
+                service_env = self.get_service_env(usages)
+                pricing_object = service_env.dummy_pricing_object  # XXX ?
+        return pricing_object
+
+    def get_usages(self, post_data):
+        usage_types = {}  # XXX
+        daily_usages = []
+        usages_daily_pricing_objects = defaultdict(list)
+
+        for usages in post_data['usages']:
+            pricing_object = self.get_pricing_object(usages)
+            for usage in usages['usages']:
+                usage_type = usage_types.get(
+                    usage['symbol'],
+                    UsageType.objects_admin.get(symbol=usage['symbol'])  # XXX try/except
+                )
+                daily_pricing_object = (
+                    pricing_object.get_daily_pricing_object(post_data['date'])
+                )
+                daily_usage = DailyUsage(
+                    date=post_data['date'],
+                    type=usage_type,
+                    value=usage['value'],
+                    daily_pricing_object=daily_pricing_object,
+                    service_environment=(
+                        daily_pricing_object.service_environment
+                    ),
+                )
+                daily_usages.append(daily_usage)
+                usages_daily_pricing_objects[usage_type].append(
+                    daily_pricing_object
+                )
+        return (daily_usages, usages_daily_pricing_objects)
+
+    def save_usages(self, post_data):
+        logger.info("Saving usages for service {}".format(
+            post_data['pricing_service']
+        ))
+        daily_usages, usages_daily_pricing_objects = self.get_usages(post_data)
+        self.remove_previous_daily_usages(
+            post_data.get('overwrite'),
+            post_data['date'],
+            usages_daily_pricing_objects,
+        )
+        DailyUsage.objects.bulk_create(daily_usages)
--- src/ralph_scrooge/tests/rest/test_pricing_service_usages.py
+++ src/ralph_scrooge/tests/rest/test_pricing_service_usages.py
@@ -7,50 +7,167 @@ from __future__ import unicode_literals
 
 import datetime
 import json
+from copy import deepcopy
 
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
-from ralph_scrooge.rest.components import ComponentsContent
+from ralph_scrooge.models import DailyUsage, ServiceUsageTypes
+# from ralph_scrooge.rest.pricing_service_usages import (
+#     # XXX
+# )
 from ralph_scrooge.tests.utils.factory import (
-    CostDateStatusFactory,
-    DailyAssetInfoFactory,
-    DailyCostFactory,
+    PricingObjectFactory,
     PricingServiceFactory,
     ServiceEnvironmentFactory,
     UsageTypeFactory,
 )
 from rest_framework.test import APIClient
 
-USAGE_TEMPLATE_1 = {
+
+PRICING_SERVICE_USAGE_TEMPLATE = {
     "pricing_service": "service-1",
     "date": "2016-09-08",
+    "usages": [],
+}
+
+USAGE_TEMPLATE_PRICING_OBJECT = {
+    "pricing_object": "some.hostname.net",
     "usages": [
         {
-            "pricing_object": "some.hostname.net",
-            "usages": [
-                {
-                    "symbol": "symbol-1",
-                    "value": 40
-                }
-            ]
+            "symbol": "symbol-1",
+            "value": 40
+        }  # add more of them if needed
+    ]
+}
+
+USAGE_TEMPLATE_SERVICE = {
+    "service": "service1",
+    "environment": "env1",
+    "usages": [
+        {
+            "symbol": "requests",
+            "value": 123
         }
     ]
 }
 
+USAGE_TEMPLATE_SERVICE_ID = {
+    "service_id": 1,
+    "environment": "env1",
+    "usages": [
+        {
+            "symbol": "requests",
+            "value": 123
+        }
+    ]
+}
+
+USAGE_TEMPLATE_SERVICE_UID = {
+    "service_uid": "sc-123",
+    "environment": "env1",
+    "usages": [
+        {
+            "symbol": "requests",
+            "value": 123
+        }
+    ]
+}
+
+
 class TestPricingServiceUsages(TestCase):
 
+    def compare_usages(self):
+        daily_usage1 = DailyUsage.objects.order_by('id')[0]
+        self.assertEquals(
+            daily_usage1.service_environment,
+            self.service_environment1
+        )
+        self.assertEquals(daily_usage1.date, self.date)
+        self.assertEquals(daily_usage1.type, self.usage_type1)
+        self.assertEquals(daily_usage1.value, 123)
+
     def setUp(self):
-        pass
+        self.date = datetime.date(2016, 9, 8)
+        self.today = datetime.date.today()
+        client = APIClient()
+        self.pricing_service = PricingServiceFactory()
+        self.service_environment = ServiceEnvironmentFactory()
+        self.pricing_object = PricingObjectFactory()
+        self.usage_type1 = UsageTypeFactory()
+        self.usage_type2 = UsageTypeFactory()
+        ServiceUsageTypes.objects.create(
+            usage_type=self.usage_type1,
+            pricing_service=self.pricing_service,
+            start=datetime.date.min,
+            end=datetime.date.max,
+            percent=50,
+        )
+        ServiceUsageTypes.objects.create(
+            usage_type=self.usage_type2,
+            pricing_service=self.pricing_service,
+            start=datetime.date.min,
+            end=datetime.date.max,
+            percent=50,
+        )
 
     def test_if_usage_saves_correctly(self):
-        pass
 
-    # XXX to be removed
-    def test_post(self):
-        client = APIClient()
-        resp = client.post(
+        pricing_service_usage = deepcopy(PRICING_SERVICE_USAGE_TEMPLATE)
+        pricing_object = deepcopy(USAGE_TEMPLATE_PRICING_OBJECT)
+        pricing_service_usage['usages'].append(pricing_object)
+        pricing_service_usage['usages'].append(pricing_object)
+
+        resp = self.client.post(
             reverse('pricing_service_usages'),
-            {'aaa': 'bbb'},
+            pricing_service_usage,
             format='json',
         )
+        self.assertEquals(resp.status_code, 201)
+        self.compare_usages()
+
+
+    # def test_if_usage_saves_correctly_with_existing_pricing_service(self):
+    #     pass
+
+    # def test_for_error_when_invalid_pricing_service_given(self):
+    #     pass
+
+    # def test_for_error_when_invalid_service_given(self):
+    #     pass
+
+    # def test_for_error_when_invalid_environment_given(self):
+    #     pass
+
+    # def test_for_error_when_service_is_missing(self):
+    #     pass
+
+    # def test_for_error_when_environment_is_missing(self):
+    #     pass
+
+    # def test_for_error_when_pricing_object_is_missing(self):
+    #     pass
+
+    # def test_for_error_when_invalid_usage_given(self):
+    #     pass
+
+    # def test_overwrite_values_only(self):
+    #     pass
+
+    # def test_overwrite_delete_all_previous(self):
+    #     pass
+
+    # def test_not_overwriting(self):
+    #     pass
+
+    # def test_get_pricing_service_usages(self):
+    #     pass
+
+    # # # XXX to be removed
+    # # def test_post(self):
+    # #     client = APIClient()
+    # #     resp = client.post(
+    # #         reverse('pricing_service_usages'),
+    # #         {'aaa': 'bbb'},
+    # #         format='json',
+    # #     )
