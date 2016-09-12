b003becb407e3c1f97d416bac9c39247bd67c0e6
Tomasz Mieszkowski <tomasz.mieszkowski@allegrogroup.com>
2016-09-12 13:58:10 +0200

    wip #5

--- src/ralph_scrooge/rest/pricing_service_usages.py
+++ src/ralph_scrooge/rest/pricing_service_usages.py
@@ -11,6 +11,7 @@ import logging
 from collections import defaultdict
 
 from rest_framework import status
+from rest_framework import serializers
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.serializers import Serializer
@@ -26,12 +27,77 @@ from ralph_scrooge.models import (
 logger = logging.getLogger(__name__)
 
 
+class UsageObject(object):
+    symbol = None
+    value = None
+
+    def __init__(self, symbol=None, value=None, **kwargs):
+        self.symbol = symbol
+        self.value = value
+
+
+class UsageObjectSerializer(Serializer):
+    symbol = serializers.CharField()
+    value = serializers.FloatField()
+
+    class Meta:
+        model = UsageObject
+
+
+class UsageObjectDeserializer(UsageObjectSerializer):
+    """Dummy class provided only for symmetry."""
+    pass
+
+
+class UsagesObject(object):
+    service = None
+    service_id = None
+    service_uid = None
+    environment = None
+    pricing_object = None
+    usages = []  # list of UsageObject
+
+    def __init__(
+        self,
+        service=None,
+        service_id=None,
+        service_uid=None,
+        environment=None,
+        pricing_object=None,
+        usages=None,
+        **kwargs
+    ):
+        self.service = service
+        self.service_id = service_id
+        self.service_uid = service_uid
+        self.environment = environment
+        self.pricing_object = pricing_object
+        self.usages = usages or []
+
+
+class UsagesObjectSerializer(Serializer):
+    service = serializers.CharField()
+    service_id = serializers.CharField()  # XXX or maybe some numeric field..?
+    service_uid = serializers.CharField()
+    environment = serializers.CharField()
+    pricing_object = serializers.CharField()
+    usages = UsageObjectSerializer(many=True)
+
+    class Meta:
+        model = UsagesObject
+        exclude = ('service_uid',)
+
+
+class UsagesObjectDeserializer(UsagesObjectSerializer):
+    usages = UsageObjectDeserializer(many=True)
+
+    class Meta:
+        model = UsagesObject
+
+
 class PricingServiceUsageObject(object):
-    """
-    Container for pricing service resources usages per service
-    """
     pricing_service = None
-    pricing_service_id = None  # XXX ?
+    pricing_service_id = None
     date = None
     overwrite = None
     usages = []  # list of UsagesObject
@@ -50,25 +116,40 @@ class PricingServiceUsageObject(object):
         self.usages = usages or []
         self.overwrite = 'no'  # default overwrite
 
-    # def to_dict(self, exclude_empty=False):
-    #     result = {
-    #         'date': self.date,
-    #         'pricing_service': self.pricing_service,
-    #         'pricing_service_id': self.pricing_service_id,
-    #         'overwrite': self.overwrite,
-    #         'usages': [u.to_dict(exclude_empty) for u in self.usages]
-    #     }
-    #     return result
-
 
 class PricingServiceUsageObjectSerializer(Serializer):
-    pricing_service = None
-    date = None
-    overwrite = None
-    usages = []  # list of UsagesObject
+    pricing_service = serializers.CharField()
+    pricing_service_id = serializers.CharField()  # XXX or maybe some numeric field..?
+    date = serializers.DateField()
+    overwrite = serializers.CharField()
+    usages = UsagesObjectSerializer(many=True)
+
+    class Meta:
+        model = PricingServiceUsageObject
+        exclude = ('overwrite',)
+
+
+class PricingServiceUsageObjectDeserializer(PricingServiceUsageObjectSerializer):  # noqa
+    usages = UsagesObjectDeserializer(many=True)
+
+    class Meta:
+        model = PricingServiceUsageObject
 
 
 class PricingServiceUsages(APIView):
+
+    # def get_serializer_class(self):
+    #     # XXX add some comment re: serializers/deserializers hack
+    #     import ipdb; ipdb.set_trace()
+    #     pass
+
+    def get(self, request, *args, **kwargs):
+        usages_date = datetime.datetime.strptime(
+            kwargs['date'],
+            '%Y-%m-%d'
+        ).date()
+        result = self.get_as_obj(usages_date, kwargs['pricing_service_id'])
+        return Response(PricingServiceUsageObjectSerializer(result).data)  # XXX
 
     def post(self, request, *args, **kwargs):
         post_data = json.loads(request.raw_post_data)
@@ -80,13 +161,7 @@ class PricingServiceUsages(APIView):
         }  # XXX ?
         return Response(result, status=status.HTTP_201_CREATED)
 
-    def get(self, request, *args, **kwargs):
-        usages_date = datetime.datetime.strptime(
-            kwargs['date'],
-            '%Y-%m-%d'
-        ).date()
-        pricing_service_id = kwargs['pricing_service_id']
-        # XXX what about wrapping this into some function?
+    def get_as_dict(self, usages_date, pricing_service_id):
         # pricing_service = PricingService.objects_admin.get(  # XXX must be admin..?
         pricing_service = PricingService.objects.get(  # XXX must be admin..?
             id=pricing_service_id,
@@ -130,7 +205,8 @@ class PricingServiceUsages(APIView):
             if se.dummy_pricing_object != dpo.pricing_object:
                 usages['pricing_object'] = dpo.pricing_object.name
             # usages.usages = [UsageObject(k, v) for k, v in u]  # XXX
-            usages['usages'] = [{'symbol': k, 'value': v} for k, v in u]  # XXX
+            # usages['usages'] = [{'symbol': k, 'value': v} for k, v in u]  # XXX
+            usages['usages'] = [UsageObjectSerializer(UsageObject(k, v)).data for k, v in u]  # XXX
             ps['usages'].append(usages)
             # XXX missing:
             # pricing_service_id (np. null)
@@ -139,7 +215,48 @@ class PricingServiceUsages(APIView):
         ps['pricing_service_id'] = None
         ps['date'] = ps['date'].strftime('%Y-%m-%d')
         # result = json.dumps(ps)
-        return Response(ps)
+        return ps
+
+    def get_as_obj(self, usages_date, pricing_service_id):
+        pricing_service = PricingService.objects.get(  # XXX must be admin..?
+            id=pricing_service_id,
+        )  # XXX try/except
+        ps = PricingServiceUsageObject(
+            pricing_service=pricing_service.name,
+            date=usages_date,
+            usages=[],
+        )
+        usages_dict = defaultdict(list)
+
+        # iterate through pricing service usage types
+        for usage_type in pricing_service.usage_types.all():
+            daily_usages = usage_type.dailyusage_set.filter(
+                date=usages_date
+            ).select_related(
+                'service_environment',
+                'service_environment__service',
+                'service_environment__environment',
+                'pricing_object'
+            )
+            for daily_usage in daily_usages:
+                usages_dict[daily_usage.daily_pricing_object].append(
+                    (daily_usage.type.symbol, daily_usage.value)
+                )
+
+        # save usages tp UsagesObjects
+        for dpo, u in usages_dict.iteritems():
+            usages = UsagesObject()
+            se = dpo.service_environment
+            usages.service = se.service.name
+            usages.service_id = se.service.id
+            usages.environment = se.environment.name
+            # if pricing object is not dummy pricing object for service
+            # environment use it
+            if se.dummy_pricing_object != dpo.pricing_object:
+                usages.pricing_object = dpo.pricing_object.name
+            usages.usages = [UsageObject(k, v) for k, v in u]
+            ps.usages.append(usages)
+        return ps
 
 
     def validate(self, post_data):  # XXX change name of this fn
