0f5040c25302468c2bf68ad8a176bc4b8c9843e2
Tomasz Mieszkowski <tomasz.mieszkowski@allegrogroup.com>
2016-09-28 13:35:38 +0200

    wip #3

--- src/ralph_scrooge/rest/pricing_service_usages.py
+++ src/ralph_scrooge/rest/pricing_service_usages.py
@@ -18,13 +18,6 @@ from rest_framework.decorators import (
     authentication_classes,
     permission_classes,
 )
-from rest_framework.exceptions import (
-    AuthenticationFailed
-)
-from rest_framework.authentication import (
-    TokenAuthentication,
-    get_authorization_header,
-)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.serializers import Serializer
@@ -37,6 +30,7 @@ from ralph_scrooge.models import (
     ServiceEnvironment,
     UsageType,
 )
+from ralph_scrooge.rest.auth import TastyPieLikeTokenAuthentication
 
 logger = logging.getLogger(__name__)
 
@@ -304,52 +298,6 @@ class PricingServiceUsageDeserializer(PricingServiceUsageSerializer):
                 "Unknown service name: {}".format(value)
             )
         return attrs
-
-
-class TastyPieLikeTokenAuthentication(TokenAuthentication):
-    """
-    A subclass of TokenAuthentication, which also supports TastyPie-like
-    auth tokens, i.e.:
-
-        Authorization: ApiKey username:401f7ac837da42b97f613d789819ff93537bee6a
-
-    and
-
-        Authorization: Token 401f7ac837da42b97f613d789819ff93537bee6a
-
-    instead of just the latter (DjRF-like). In the former case, the "username:"
-    component is silently discarded.
-    """
-    keywords = ('ApiKey', 'Token')
-
-    def authenticate(self, request):
-        auth = get_authorization_header(request).split()
-
-        for keyword in self.keywords:
-            if auth and auth[0].lower() == keyword.lower().encode():
-                break
-        else:
-            return None
-
-        if len(auth) == 1:
-            msg = 'Invalid token header. No credentials provided.'
-            raise AuthenticationFailed(msg)
-        elif len(auth) > 2:
-            msg = (
-                'Invalid token header. Token string should not contain spaces.'
-            )
-            raise AuthenticationFailed(msg)
-
-        try:
-            token = auth[1].split(':')[-1].decode()
-        except UnicodeError:
-            msg = (
-                'Invalid token header. Token string should not contain '
-                'invalid characters.'
-            )
-            raise AuthenticationFailed(msg)
-
-        return self.authenticate_credentials(token)
 
 
 @api_view(['GET'])
--- src/ralph_scrooge/rest/team_time_division.py
+++ src/ralph_scrooge/rest/team_time_division.py
@@ -5,38 +5,27 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import json
-
+from django.db.transaction import commit_on_success
+from django.http import HttpResponse
 from rest_framework import serializers
-from rest_framework.exceptions import APIException
 from rest_framework.response import Response
 from rest_framework.serializers import Serializer
 from rest_framework.views import APIView
 
-from datetime import date, timedelta
-from dateutil.relativedelta import relativedelta
-
-from django.db.transaction import commit_on_success
-
-from ralph_scrooge.rest.common import get_dates
 from ralph_scrooge.models import (
-    DailyUsage,
-    ExtraCost,
-    ExtraCostType,
-    PricingService,
     Service,
     ServiceEnvironment,
-    ServiceUsageTypes,
     Team,
     TeamCost,
     TeamServiceEnvironmentPercent,
-    UsageType,
 )
+from ralph_scrooge.rest.auth import TastyPieLikeTokenAuthentication
+from ralph_scrooge.rest.common import get_dates
 
 
 class PercentSerializer(Serializer):
     service_uid = serializers.CharField(required=True)
-    environment = serializers.CharField(required=True)  # XXX ???
+    environment = serializers.CharField(required=True)
     percent = serializers.FloatField(required=True)
 
     def validate_service_uid(self, attrs, source):
@@ -46,7 +35,6 @@ class PercentSerializer(Serializer):
             raise serializers.ValidationError(err)
         return attrs
 
-
     def validate(self, attrs):
         err = None
 
@@ -54,26 +42,16 @@ class PercentSerializer(Serializer):
         service_uid = attrs.get('service_uid')
         env = attrs.get('environment')
         if not ServiceEnvironment.objects.filter(
-            service_ci_uid=service_uid,
-            environment_name=env,
+            service__ci_uid=service_uid,
+            environment__name=env,
         ).exists():
             err = (
                 'Service environment for service with UID "{}" and '
                 'environment "{}" does not exist.'.format(service_uid, env)
             )
 
-        # validate percents (if they sum up to 100)
-        percent_total = 0
-        for d in attrs.get('division'):
-            percent_total += d['percent']
-        if percent_total < 100:
-            err = (
-                "Percents should sum to 100, now it's {}."
-                .format(percent_total)
-            )
-
         if err is not None:
-            raise serializers.ValidationError(err)  # XXX what about aggregating them?
+            raise serializers.ValidationError(err)
         return attrs
 
 
@@ -110,8 +88,32 @@ class TeamTimeDivisionSerializer(Serializer):
 
     def validate_team_id(self, attrs, source):
         team_id = attrs[source]
-        # XXX
+        try:
+            team = Team.objects.get(id=team_id)
+        except Team.DoesNotExist:
+            err = "Team with ID {} does not exist.".format(team_id)
+            raise serializers.ValidationError(err)
+        # XXX(xor-xor) I don't like filling it here (team_id vs team).
+        attrs[source] = team
         return attrs
+
+    def validate(self, attrs):
+        err = None
+
+        # validate percents (if they sum up to 100)
+        percent_total = 0
+        for d in attrs.get('division'):
+            percent_total += d['percent']
+        if percent_total < 100:
+            err = (
+                "Percents should sum to 100, now it's {}."
+                .format(percent_total)
+            )
+
+        if err is not None:
+            raise serializers.ValidationError(err)
+        return attrs
+
 
 def new_team_time_division(team_id, year, month, division):
     return {
@@ -133,38 +135,41 @@ class TeamTimeDivision(APIView):
         serializer = TeamTimeDivisionSerializer(division)
         return Response(serializer.data)
 
-    @commit_on_success()
-    def post(self, request, year, month, team, *args, **kwargs):
-        post_data = json.loads(request.raw_post_data)
-        first_day, last_day, days_in_month = get_dates(year, month)
+    def post(self, request, *args, **kwargs):
+        serializer = TeamTimeDivisionSerializer(data=request.DATA)
+        if serializer.is_valid():
+            save_team_time_division(serializer.object)
+            return HttpResponse(status=201)
+        return Response(serializer.errors, status=400)
 
-        try:
-            team = Team.objects.get(id=team)
-        except Team.DoesNotExist:
-            return {'status': False, 'message': 'Team Does Not Exist.'}
-        TeamServiceEnvironmentPercent.objects.filter(
-            team_cost__team=team,
-            team_cost__start=first_day,
-            team_cost__end=last_day,
-        ).delete()
-        team_cost = TeamCost.objects.get_or_create(
-            team=team,
-            start=first_day,
-            end=last_day,
-        )[0]
-        for row in post_data['rows']:
-            service_environment = ServiceEnvironment.objects.get(
-                service__id=row.get('service'),
-                environment__id=row.get('env')
-            )
-            tsep = TeamServiceEnvironmentPercent.objects.get_or_create(
-                team_cost=team_cost,
-                service_environment=service_environment,
-                defaults=dict(
-                    percent=row.get('value'),
-                )
-            )[0]
-            tsep.percent = row.get('value')
+
+@commit_on_success()
+def save_team_time_division(ttd):
+    first_day, last_day, days_in_month = get_dates(ttd['year'], ttd['month'])
+
+    # Previously uploaded division(s) for a given team and dates should be
+    # removed.
+    TeamServiceEnvironmentPercent.objects.filter(
+        team_cost__team=ttd['team_id'],
+        team_cost__start=first_day,
+        team_cost__end=last_day,
+    ).delete()
+
+    team_cost = TeamCost.objects.get_or_create(
+        team=ttd['team_id'],
+        start=first_day,
+        end=last_day,
+    )[0]
+    for percent in ttd['division']:
+        service_environment = ServiceEnvironment.objects.get(
+            service__ci_uid=percent.get('service_uid'),
+            environment__name=percent.get('environment')
+        )
+        tsep, created = TeamServiceEnvironmentPercent.objects.get_or_create(
+            team_cost=team_cost,
+            service_environment=service_environment,
+            defaults=dict(percent=percent.get('percent'))
+        )
+        if not created:
+            tsep.percent = percent.get('percent')
             tsep.save()
-
-        return Response({"status": True})
