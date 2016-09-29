e256733b8026c799ca5f97e1e830c038e893eee1
Mateusz Kurek <mateusz.kurek@allegrogroup.com>
2016-09-29 11:58:21 +0200

    wip

--- src/ralph_scrooge/settings/prod.py
+++ src/ralph_scrooge/settings/prod.py
@@ -5,7 +5,7 @@ from ralph_scrooge.settings.base import *  # noqa
 MEDIA_ROOT = os.path.expanduser('~/.scrooge/shared/uploads')
 STATIC_ROOT = os.path.expanduser('~/.scrooge/shared/static')
 
-INSTALLED_APPS += ('gunicorn',)
+INSTALLED_APPS += ('gunicorn',)  # noqa
 
 try:
     execfile(os.path.expanduser("~/.scrooge/settings"))  # noqa
