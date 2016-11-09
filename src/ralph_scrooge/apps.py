cac0247dac14f4add842f6f7a462953a4688f150
Mateusz Kurek <mkurek@users.noreply.github.com>
2016-11-09 15:37:00 +0100

    Remove populating user flags (#545)
    There is no need to populate user flags on our own - if you need to populate 'is_active', 'is_staff' and 'is_superuser' use AUTH_LDAP_PROFILE_FLAGS_BY_GROUP settings (see https://pythonhosted.org/django-auth-ldap/users.html#easy-attributes for details).

--- src/ralph_scrooge/apps.py
+++ src/ralph_scrooge/apps.py
@@ -6,7 +6,3 @@ from django.utils.translation import ugettext_lazy as _
 class ScroogeAppConfig(AppConfig):
     name = 'ralph_scrooge'
     verbose_name = _('Scrooge')
-
-    def ready(self):
-        from ralph_scrooge.utils.ldap import register_callbacks
-        register_callbacks()
