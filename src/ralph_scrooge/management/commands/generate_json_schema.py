974aad48e05d102d0a802e39fc4bab007ccb5185
Tomasz Mieszkowski <tomasz.mieszkowski@allegrogroup.com>
2016-11-28 12:31:13 +0100

    hail flake

--- src/ralph_scrooge/management/commands/generate_json_schema.py
+++ src/ralph_scrooge/management/commands/generate_json_schema.py
@@ -15,6 +15,7 @@ from django.core.management.base import BaseCommand
 
 logger = logging.getLogger(__name__)
 
+
 class Command(BaseCommand):
     """Generates JSON schema file from it's YAML counterpart identified by
     `API_SCHEMA_FILE`, which is expected to be present in settings. Previous
