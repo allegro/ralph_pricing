d65ce4d56011ebf11826889a429253d7d0eb5aca
Tomasz Mieszkowski <tomasz.mieszkowski@allegrogroup.com>
2016-12-09 11:45:42 +0100

    add endpoint for PricingService

--- src/ralph_scrooge/rest_api/public/v0_10/router.py
+++ src/ralph_scrooge/rest_api/public/v0_10/router.py
@@ -6,10 +6,14 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from rest_framework import routers
 
-from ralph_scrooge.rest_api.public.v0_10.views import UsageTypesViewSet
+from ralph_scrooge.rest_api.public.v0_10.views import (
+    UsageTypesViewSet,
+    PricingServicesViewSet,
+)
 
 
 router = routers.SimpleRouter()
 
 router.register(r'usage-types', UsageTypesViewSet)
+router.register(r'pricing-services', PricingServicesViewSet)
 urlpatterns = router.urls
